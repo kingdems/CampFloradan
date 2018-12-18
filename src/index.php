02e9b65aa749e9e2ba6d14b7766a2536397e3554
Jacob deMey <demeyjacob@gmail.com>
2018-12-18 14:17:11 -0500

    edited login

--- src/index.php
+++ src/index.php
@@ -16,14 +16,12 @@ if (!$conn) {
 $_SESSION['user'] = $username;
 
 //$pwd = crypt($pwd, '$2a$07$usesomesillystringforsalt$');
-$sql = "SELECT ID FROM users WHERE username = '$username' and pwd = '$pwd'";
+$sql = "SELECT ID FROM users WHERE username = '$username' and password = '$pwd'";
 
-var_dump($pwd);
-var_dump($username);
-var_dump($sql);
+
 
 $result = mysqli_query($conn,$sql);
-var_dump($result);
+
 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 
 $count = mysqli_num_rows($result);
