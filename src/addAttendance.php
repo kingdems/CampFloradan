202d2a22a81f131ff2a1fdd716a878628a1c58a5
Jacob deMey <demeyjacob@gmail.com>
2019-01-28 21:03:46 -0500

    change PHP code

--- src/addAttendance.php
+++ src/addAttendance.php
@@ -35,7 +35,7 @@ if (!$conn) {
        if(mysqli_num_rows($data) > 0) {
             $data_row = mysqli_fetch_assoc($data);
 
-            foreach($data_row){
+            while($data_row){
              echo     '<tr>';
              echo        '<td height="50">' . $data_row["fname"] . '</td>';
              echo        ' <td height="50">'. $data_row["lname"] . '</td>';
