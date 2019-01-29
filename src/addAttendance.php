cbe76c8afafa3eca0bc31efac2e7d20ed097b6a8
Jacob deMey <demeyjacob@gmail.com>
2019-01-28 21:03:19 -0500

    change PHP code

--- src/addAttendance.php
+++ src/addAttendance.php
@@ -35,10 +35,10 @@ if (!$conn) {
        if(mysqli_num_rows($data) > 0) {
             $data_row = mysqli_fetch_assoc($data);
 
-            foreach($data_row as $value){
+            foreach($data_row){
              echo     '<tr>';
-             echo        '<td height="50">' . $value . '</td>';
-             echo        ' <td height="50">'. $value . '</td>';
+             echo        '<td height="50">' . $data_row["fname"] . '</td>';
+             echo        ' <td height="50">'. $data_row["lname"] . '</td>';
              echo        ' <td height="50">' ?> <input type = "checkbox" name="att" value=1 />  <?php echo '</td>';
              echo    ' </tr>';
        }
