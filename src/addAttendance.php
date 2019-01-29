3564397c0005b518dd6a01ab3366b1eb4cfc1c88
Jacob deMey <demeyjacob@gmail.com>
2019-01-29 13:37:37 -0500

    change PHP code

--- src/addAttendance.php
+++ src/addAttendance.php
@@ -39,7 +39,7 @@ if (!$conn) {
              echo     '<tr>';
              echo        '<td height="50">' . $row["fname"] . '</td>';
              echo        ' <td height="50">'. $row["lname"] . '</td>';
-             echo        ' <td height="50">' ?> <input type = "checkbox" name="att[]" value=1 />  <?php echo '</td>';
+             echo        ' <td height="50">' ?> <input type = "checkbox" name="att[]" value=1 /><label>PRESENT</label>  <?php echo '</td>';
              echo    ' </tr>';
        }
        }
