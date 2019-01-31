eef5ac7b69cb82e07f9da69f3821199b6e9300ae
Jacob deMey <demeyjacob@gmail.com>
2019-01-31 15:45:03 -0500

    change PHP code

--- src/expenses.php
+++ src/expenses.php
@@ -79,11 +79,93 @@ if (!$conn) {
 
        if(mysqli_num_rows($data) > 0) {
             $data_row = mysqli_fetch_assoc($data);
+            $col_name = "";
+            $sql = "SELECT * from expenses WHERE $col_name IS NOT NULL";
+
+            for($i = 0; $i < 10; $i++){
+
+                switch(i){
+                    case 1:
+                        $col_group = 'drivers';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                            }
+                        break;
+                    case 2:
+                        $col_group = 'nurses';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                        }
+                        break;
+                    case 3:
+                        $col_group = 'salaryAdvance';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                        }
+                        break;
+                    case 4:
+                        $col_group = 'snack';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                        }
+                        break;
+                    case 5:
+                        $col_group = 'rainyDay';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                        }
+                        break;
+                    case 6:
+                        $col_group = 'gas';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                        }
+                        break;
+                    case 7:
+                        $col_group = 'bbq';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                        }
+                        break;
+                    case 8:
+                        $col_group = 'sports';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                        }
+                        break;
+                    case 9:
+                        $col_group = 'artsAndCrafts';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                        }
+                        break;
+                    case 10:
+                        $col_group = 'drivers';
+                        $column = mysqli_query($conn, $sql);
+                        if($column != NULL){
+                            $info = $column;
+                        }
+                        break;
+                    default:
+                        echo 'ALL NULL';
+                        break;
+
+
+            }
 
             for($x = 0; $x <1; $x++){
              echo     '<tr>';
              echo        '<td height="50">' . $data_row['entryDate'] . '</td>';
-             echo        ' <td height="50">'. $data_row['expID'] . '</td>';
+             echo        ' <td height="50">'. $data_row[$info] . '</td>';
              echo        ' <td height="50">'. $data_row['bbq'] . $data_row['drivers'] . '</td>';
              echo    ' </tr>';
        }
