faf7986b94f91c56f1bc9d11e988824bd07e7e7d
Jacob deMey <demeyjacob@gmail.com>
2019-02-01 15:53:09 -0500

    change PHP code

--- src/expenses.php
+++ src/expenses.php
@@ -79,7 +79,7 @@ if (!$conn) {
 
        if(mysqli_num_rows($data) > 0) {
             $data_row = mysqli_fetch_assoc($data);
-
+            $col_group = '';
 
             for($i = 1; $i < 10; $i++){
                 $sql = "SELECT * from expenses WHERE $col_group IS NOT NULL";
